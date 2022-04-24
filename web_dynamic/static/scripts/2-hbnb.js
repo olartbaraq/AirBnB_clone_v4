cea998e66ed6673a57c27094a80b7bd1c03910b6
olart <akanbi.ges201@gmail.com>
2022-04-24 16:40:59 +0100

    chnaged api address

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 	    $('div.amenities > h4').html('&nbsp;');
 	}
     });
-    $.get("http://127.0.0.1:5001/api/v1/status/", function(data, docStatus) {
+    $.get("http://0.0.0.0:5001/api/v1/status/", function(data, docStatus) {
       if (docStatus === 'success') {
 	if (data.status === 'OK') {
 	  $('#api_status').addClass('available');
